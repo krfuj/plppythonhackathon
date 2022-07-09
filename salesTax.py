1144989c75802892460882bdfa6997efccac9647
Sakwa Presley <59162548+krfuj@users.noreply.github.com>
2022-07-09 18:38:34 +0300

    edited sales task challenge to add cost of paint

--- salesTax.py
+++ salesTax.py
@@ -5,9 +5,12 @@ labour_hourlyCost = 20
 
 
 userWall_space = int(input("Enter a your wallspace: "))
+price_paint = int(input("Enter price of paint per galon: "))
 needed_gallonPaint = userWall_space / 115
+cost_paint = needed_gallonPaint * price_paint
 hours_required = needed_gallonPaint * 8
 Overal_LabourCost = hours_required * labour_hourlyCost
+total_cost = cost_paint + Overal_LabourCost
 
-print(f"For {userWall_space} square foot will need {needed_gallonPaint} gallons of paint, {hours_required} hours to complete, {Overal_LabourCost} for labour with cost")
+print(f"For {userWall_space} square foot will need {needed_gallonPaint} gallons of paint, {hours_required} hours to complete, the cost of paint being ksh.{cost_paint}. The labour cost will stand at {Overal_LabourCost} for labour with cost making the overrall cost of the paint job be ksh.{total_cost}")
 
